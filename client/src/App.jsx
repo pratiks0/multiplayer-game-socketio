abae02b66e04cde836e1ffb3c6ae00a5f8ee9a3e
Pratik Singh <pratik00888@gmail.com>
2024-11-11 22:38:23 +0530

    frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
 import './App.css'
+import Square from './square/Square'
+
+const renderFrom = [
+  [1,2,3],
+  [4,5,6],
+  [7,8,9]
+]
 
 const App = () => {
   return (
-    <div>
-      hello
+    <div className='body'>
+    <div className='main-div'>
+      <div>
+        <div className="move-detection">
+          <div className="left">You</div>
+          <div className="right">Opponent</div>
+        </div>
+        <h1 className='heading'>Tic Tac Toe</h1>
+        <div className="square-wrapper">
+          {
+          renderFrom.map(arr =>
+          arr.map(e => {
+            return <Square/>
+          })
+          )
+          }
+        </div>
+      </div>
+    </div>
     </div>
   )
 }
---
+++ client/src/square/Square.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Square.css'
+
+const Square = () => {
+  return (
+    <div className='square'>
+      
+    </div>
+  )
+}
+
+export default Square
