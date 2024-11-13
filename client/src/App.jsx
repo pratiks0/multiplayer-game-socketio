e99fa7d84b0feabc32959fd4fd88fae4c5fade41
Pratik Singh <pratik00888@gmail.com>
2024-11-13 12:58:30 +0530

    final commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -128,7 +128,7 @@ const App = () => {
     const username = result.value;
     setPlayerName(username);
 
-    const newSocket = io(`${window.location.origin}`, {
+    const newSocket = io(`http://localhost:3000`, {
       autoConnect: true,
     });
 
